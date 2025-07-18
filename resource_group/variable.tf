add1271acba3781e9318ad4c2b334f27eba6f870
Satya <satyaprakashjh7@gmail.com>
2025-07-18 22:42:09 +0530

    hrllo kaise hoo app

--- resource_group/variable.tf
+++ resource_group/variable.tf
@@ -2,6 +2,13 @@ variable "resource_group_name" {
   type        = string
 }
 
+
 variable "resource_group_location" {
   type        = string 
 }
+
+# # variable "resource_group_tags" {
+#   type        = map(string)
+#   default     = {}
+#   description = "Tags to be applied to the resource group"
+# }
